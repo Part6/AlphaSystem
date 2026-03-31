c35606ff80395899aed507897973317aa43e81b8
Gustavo Bertassi <gustavobertassi6@gmail.com>
2026-03-31 17:39:43 -0300

    Criação do projeto, assim como classes a serem usadas e as telas do sistema

---
+++ src/alphasystem/AlphaSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alphasystem;
+import gui.LoginTela;
+/**
+ *
+ * @author Desktop
+ */
+public class AlphaSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LoginTela().setVisible(true);
+    }
+    
+}
---
+++ src/persistencia/Cliente.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persistencia;
+
+/**
+ *
+ * @author Desktop
+ */
+public class Cliente {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private Endereco endereco;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public Endereco getEndereco() { return endereco; }
+    public void setEndereco(Endereco endereco) { this.endereco = endereco; }
+}
